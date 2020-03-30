05d99999433d2ca9468efc7c81e7de433951d717
sungwoo_chang <hwac1102@gmail.com>
2020-03-31 00:12:14 +0900

    fetch 추가

--- src/widgets/Currency.jsx
+++ src/widgets/Currency.jsx
@@ -1,26 +1,53 @@
 import React from 'react';
 
 const authKey = 'VFLd2hbXHzciwgwcVXyXd81BFYpZD64M';
-const command = `curl -s https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${authKey}&data=AP01`;
+const url = `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${authKey}&data=AP01`;
 
-function onChange() {
+function Currency() {
+  const onChange = () => (
+    <div>
+      onChange
+    </div>
+  );
 
-}
+  const handleClick = () => (
+    alert('not implemented!')
+  );
 
-export default function Currency() {
+  fetch(url, { mode: 'no-cors' })
+    .then((response) => {
+      response.text();
+    })
+    .then((data) => (
+      (data ? JSON.parse(data) : {})
+    ))
+    .then((data) => {
+      const select = document.getElementById('currencyName');
+      const currencyInfo = data.json();
+      currencyInfo.forEach((currency, i) => {
+        select.innerHTML = `${select.innerHTML}
+            <option value="${currency[i].cur_unit}">${currency[i].cur_nm}</option>`;
+      });
+    })
+    .catch((error) => {
+    });
+
   return (
     <div>
       <p>
         <span>환전 금액: </span>
-        <input/>
+        <input />
       </p>
       <p>
         <span>환전 통화: </span>
-        <select name="currencyName" id="currencyName" onChange="onChange();">
-          <option value="X" selected>--선택--</option>
+        <select name="currencyName" id="currencyName" onChange={() => onChange()} defaultValue="X">
+          <option value="X">--선택--</option>
         </select>
       </p>
-      <button>환전하기</button>
+      <button type="button" onClick={() => handleClick()}>환전하기</button>
     </div>
+
   );
 }
+
+export default Currency;
