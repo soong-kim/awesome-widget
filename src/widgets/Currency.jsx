4d9c6041c1e1415f624ed0add3ce57cd57fc1522
sungwoo_chang <hwac1102@gmail.com>
2020-04-08 19:59:27 +0900

    Currency useRef로 수정

--- src/widgets/Currency.jsx
+++ src/widgets/Currency.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 const CurrencyList = (props) => {
@@ -33,26 +33,27 @@ CurrencyList.propTypes = {
 };
 
 const Currency = () => {
-  const [nationFrom, setFrom] = useState('X');
-  const [nationTo, setTo] = useState('X');
   const [ratio, setRatio] = useState(1);
   const [input, setInput] = useState(0);
   const baseUrl = 'https://earthquake.kr:23490/query';
+  const nationFrom = useRef('X');
+  const nationTo = useRef('X');
 
   const updateRatio = () => {
-    if (nationFrom !== 'X' && nationTo !== 'X') {
-      const searchKey = `${nationFrom}${nationTo}`;
+    if (nationFrom.current !== 'X' && nationTo.current !== 'X') {
+      const searchKey = `${nationFrom.current}${nationTo.current}`;
       const url = `${baseUrl}/${searchKey}`;
+      window.console.log(searchKey);
       fetch(url)
         .then((res) => res.json())
         .then((json) => {
           if (Object.prototype.hasOwnProperty.call(json, searchKey)) {
             setRatio(json[searchKey][0]);
           } else {
-            window.alert('제공하지 않는 서비스입니다.');
+            setRatio(0);
           }
         })
-        .catch((res) => console.error(res));
+        .catch((res) => window.console.error(res));
     }
   };
 
@@ -60,15 +61,14 @@ const Currency = () => {
     const { value } = e.target.options[e.target.selectedIndex];
     const { id } = e.target;
     if (id === 'options_from') {
-      setFrom(value);
+      nationFrom.current = value;
     } else {
-      setTo(value);
+      nationTo.current = value;
     }
     updateRatio();
   };
 
   const handleInput = (e) => {
-    updateRatio();
     setInput(e.target.value);
   };
 
@@ -79,7 +79,7 @@ const Currency = () => {
       <input type="text" id="amount_from" size="20" placeholder="0" onChange={(e) => handleInput(e)} />
       <h4> To </h4>
       <CurrencyList id="options_to" handleOption={handleSelected} />
-      <input type="text" id="result" value={ratio * input} readOnly />
+      <input type="text" id="result" value={ratio > 0 ? ratio * input : '지원하지 않는 서비스입니다.'} readOnly />
     </div>
   );
 };
